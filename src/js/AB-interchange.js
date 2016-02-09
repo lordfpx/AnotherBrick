0fba3107eaff1cea4c3d5a961a3c49c4c357acc7
Thierry Philippe <tphilippe@bi.local>
2016-02-09 14:17:55 +0100

    [interchange] fix breakpoint not listed

--- src/js/AB-interchange.js
+++ src/js/AB-interchange.js
@@ -18,7 +18,7 @@ var Interchange = function(element, opt) {
   this.rules = [];
 
   this.init()
-      .events();
+      ._events();
 };
 
 Interchange.defaults = {
@@ -44,7 +44,7 @@ Interchange.prototype = {
       rules = this.$element.data('ab-interchange').match(/\[.*?\]/g);
     }
 
-    for (var i=0, len=rules.length; i<len; i++) {
+    for (var i = 0, len = rules.length; i < len; i++) {
       var rule = rules[i].slice(1, -1).split(', '),
           path = rule.slice(0, -1).join(''),
           query = rule[rule.length - 1];
@@ -62,23 +62,29 @@ Interchange.prototype = {
 
   _reflow: function() {
     var match,
+        path,
         currentQuery = AB.mediaQuery.current;
 
     // Iterate through each rule
-    for (var i=0, len=this.rules.length; i<len; i++) {
+    for (var i = 0, len = this.rules.length; i < len; i++) {
       var rule = this.rules[i];
 
-      if (rule.query === currentQuery) {
-        this.replace(rule.path);
+      if ( window.matchMedia(AB.mediaQuery.get(rule.query)).matches ) {
+        path = rule.path;
+        match = true;
       }
+    }
+
+    if (match) {
+      this._replace(path);
     }
   },
 
-  events: function() {
+  _events: function() {
     $(window).on('resize.ab.interchange', this._reflow.bind(this));
   },
 
-  replace: function(path) {
+  _replace: function(path) {
     if (this.currentPath === path) return;
 
     var that = this,
